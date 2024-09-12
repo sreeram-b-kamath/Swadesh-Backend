15b4a6cbc2486211a0edc0ac435fc8df4cddbf4f
fardeen-hash <needraf007@gmail.com>
2024-09-12 12:25:45 +0530

    initial

--- Swadesh-Backend/Swadesh-Backend/MappingConfig.cs
+++ Swadesh-Backend/Swadesh-Backend/MappingConfig.cs
@@ -15,7 +15,10 @@ namespace Swadesh_Backend
             CreateMap<Restaurant, RestuarantUserGetDto>().ReverseMap();
             CreateMap<MenuItem, PostToMenuDto>().ReverseMap();
             CreateMap<MenuItem, MenuItemResponse>()
-            .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.MenuCategory.Name));
+            .ForMember(dest => dest.CategoryName, opt => opt.MapFrom(src => src.MenuCategory.Name)).
+             ForMember(dest => dest.Ingredients, opt => opt.MapFrom(src => src.MenuItemIngredients.Select(mii => mii.Ingredients)));
+            CreateMap<Ingredients, IngredientResponse>()
+                .ForMember(dest => dest.Image, opt => opt.MapFrom(src => src.image));
             CreateMap<MenuCategory, CategoryMenuItemsResponseDto>();
             CreateMap<MenuItem, GetMenuItemDto>()
                 .ForMember(dest => dest.IngredientIds, opt => opt.MapFrom(src => src.MenuItemIngredients.Select(mi => mi.IngredientId).ToArray()));
