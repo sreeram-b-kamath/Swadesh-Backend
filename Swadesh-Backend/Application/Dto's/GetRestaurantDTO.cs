a29284a9fba132da8fff4af59248bef956b2ab19
SREERAM B KAMATH <srambiju@gmail.com>
2024-09-17 11:37:17 +0530

    added admin API's

---
+++ Swadesh-Backend/Application/Dto's/GetRestaurantDTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Dto_s
+{
+    public class GetRestaurantDTO
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Email { get; set; }
+        public string? Logo { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
